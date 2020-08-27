c10e0dd005f1b4f4072e3f066f6ac33ead14fac9
jrhong95 <jrhong95@gmail.com>
2020-08-27 12:33:25 +0900

    Revise 200827_Find_the_odd_int.py

--- week_01/code_wars/shhong/200827_Find _the _odd_int.py
+++ week_01/code_wars/shhong/200827_Find _the _odd_int.py
@@ -1,5 +1,4 @@
-from functools import reduce
-def find_it(xs):
-    return reduce(lambda x, y: x ^ y, xs)
-
-print(find_it([5,4,3,2,1,5,4,3,2,10,10]))
+def find_it(seq):
+    for n in seq:
+        if seq.count(n) % 2:
+            return n
