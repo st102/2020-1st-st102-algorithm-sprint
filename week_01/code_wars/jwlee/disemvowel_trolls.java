facb1fc0cc3ee9d34ff00cac589ca3f9d850a261
Jiwon Lee <wonee0321@gmail.com>
2020-08-30 04:01:24 +0900

    Modify disemvowel_trolls.java

--- week_01/code_wars/jwlee/disemvowel_trolls.java
+++ week_01/code_wars/jwlee/disemvowel_trolls.java
@@ -2,8 +2,6 @@ public class Troll {
     public static String disemvowel(String str) {
         
       StringBuilder result = new StringBuilder();
-      int index = 0;
-      char[] save = new char[str.length()];
       
       for(char k : str.toCharArray()) {
         switch(k) {
@@ -19,12 +17,11 @@ public class Troll {
             case 'U' :
             continue;
         }
-          save[index++] = k; 
        
         result.append(k);
       }
       
-          return String.valueOf(result);
+          return result.toString();
       
          
     }
