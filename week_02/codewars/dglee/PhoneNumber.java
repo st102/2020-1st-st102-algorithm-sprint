771cc9e8e3c2ec90ce4dc2de67419d74f5d9556c
rkdmf1026 <rkdmf1026@naver.com>
2020-09-02 17:20:18 +0900

    add PhoneNumber.java PhoneNumber.md

---
+++ week_02/codewars/dglee/PhoneNumber.java
@@ -0,0 +1,8 @@
+package Cote_Package;
+
+public class PhoneNumber {
+	public static String createPhoneNumber(int[] numbers) {
+	    return String.format("(%d%d%d) %d%d%d-%d%d%d%d",numbers[0],numbers[1],numbers[2],numbers[3],numbers[4],numbers[5],numbers[6],numbers[7],numbers[8],numbers[9]);
+	  }
+	}
+
