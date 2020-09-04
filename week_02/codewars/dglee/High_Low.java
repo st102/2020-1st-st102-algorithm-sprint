bc2fb593a2f64950194693fa815b0481f608247d
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2020-09-04 23:03:06 +0900

    Update High_Low.java

--- week_02/codewars/dglee/High_Low.java
+++ week_02/codewars/dglee/High_Low.java
@@ -1,19 +1,11 @@
 package Cote_Package;
 import java.util.stream.*;
+import java.util.Arrays;
 public class High_Low {
     public static String highAndLow(String numbers) {
-        String output = "";
         int[] digits = Stream.of(numbers.split(" ")).mapToInt(Integer::parseInt).toArray();
-        int min = digits[0];
-        int max = digits[0];
+        Arrays.sort(digits);
 
-        for (int i = 1; i < digits.length; i++) {
-            if (min > digits[i])
-                min = digits[i];
-
-            if (max < digits[i])
-                max = digits[i];
-        }
-        return String.format("%d %d", max, min);
+        return String.format("%d %d", digits[digits.length - 1], digits[0]);
     }
 }
