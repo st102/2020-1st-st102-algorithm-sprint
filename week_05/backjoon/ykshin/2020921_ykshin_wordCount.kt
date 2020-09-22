47d3f3326ca7fc071fe10884456efef6055d828f
Kwon <syg4806@gmail.com>
2020-09-22 21:54:24 +0900

    solve word count

---
+++ week_05/backjoon/ykshin/2020921_ykshin_wordCount.kt
@@ -0,0 +1,9 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.System.`in`
+import java.util.*
+
+
+fun main() {
+    println(StringTokenizer(BufferedReader(InputStreamReader(`in`)).readLine()).countTokens())
+}
